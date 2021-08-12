8f90a604fb3b041c4509108b19c697fa71b2caa2
Flash-LHR <2479082762@qq.com>
2021-08-12 21:01:06 +0800

    add decrypt

---
+++ blockCipher/DES/decrypt/decrypt.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include "../common/F.cpp"
+#include "../common/IP.cpp"
+#include "../keySchedule/keySchedule.cpp"
+using namespace std;
+
+class decrypt {
+public:
+    static vector<bool> decryption(vector<bool> C) {
+        vector<bool> LR = IP::afterIP(C);
+        vector<bool> L(32), R(32), temp(32), RL(64), P(64);
+        for(int i = 0; i < 32; ++i) {
+            L[i] = LR[i];
+            R[i] = LR[i + 32];
+        }
+        keySchedule::genRoundKey();
+        for(int i = 0; i < 16; ++i) {
+            for(int j = 0; j < 32; ++j) temp[j] = L[j];
+            for(int j = 0; j < 32; ++j) L[j] = R[j];
+            R = F::afterF(R, keySchedule::getRoundKey(16 - i));
+            for(int j = 0; j < 32; ++j) R[j] = R[j]^temp[j];
+        }
+        for(int i = 0; i < 32; ++i) {
+            RL[i] = R[i];
+            RL[i + 32] = L[i];
+        }
+        P = IP::afterIPInv(RL);
+        return P;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    if(argc != 2 || strlen(argv[1]) != 64) {
+        cout << "Illegal input\n";
+    } else {
+        vector<bool> C(64);
+        for(int i = 0; i < 64; ++i) C[i] = argv[1][i] - '0';
+        vector<bool> P = decrypt::decryption(C);
+        cout << "plaintext:";
+        for(int i = 0; i < 64; ++i) cout << P[i];
+        cout << "\n";
+    }
+    return 0;
+}
